8c87cd29afe2ab4ad17b97fe234d4e9538e72934
Oksana Volchek <VolchekOA@gmail.com>
2018-01-05 22:13:27 +0200

    Modified enum for programming languages

--- jwd_likeIT/src/main/java/by/tr/web/entity/Language.java
+++ jwd_likeIT/src/main/java/by/tr/web/entity/Language.java
@@ -3,8 +3,8 @@ package by.tr.web.entity;
 public enum Language {
 
 	C("C"), CPP("C++"), C_SHARP("C#"), JAVA("Java"), PYTHON("Python"), 
-			SWIFT("Swift"), PERL("Perl"), PHP("php"), HTML(
-			"HTML"), CSS("CSS"), JAVASCRIPT("JavaScript"), SQL("Sql");
+			SWIFT("Swift"), PERL("Perl"), PHP("php"), HTML("HTML"),
+			CSS("CSS"), JAVASCRIPT("JavaScript"), SQL("Sql");
 
 	private final String name;
 
